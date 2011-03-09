466a96e8f121fc80902c2756439b1fa20e20705c
Matthew Boston <matthew.boston@gmail.com>
2011-03-08 19:57:58 -0500

    Initial Commit

---
+++ src/gameoflife_clj/core.clj
@@ -0,0 +1,1 @@
+(ns gameoflife-clj.core)
---
+++ test/gameoflife_clj/test/core.clj
@@ -0,0 +1,6 @@
+(ns gameoflife-clj.test.core
+  (:use [gameoflife-clj.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
