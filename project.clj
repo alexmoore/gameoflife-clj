d972a774158dcfa127331d091342a61f22d481ab
Matthew Boston <matthew.boston@gmail.com>
2011-04-20 21:54:02 -0400

    upgrading clojure > 1.2.1 and speclj > 1.3.1

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject gameoflife-clj "1.0.0-SNAPSHOT"
   :description "Conway's Game of Life written in Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[speclj "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[speclj "1.3.1"]]
   :test-path "spec/")
